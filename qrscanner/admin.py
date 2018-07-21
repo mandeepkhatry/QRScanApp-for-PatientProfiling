b5bc6d6ce58e250f1e6a2ea4f88ea9abb864abdc
mandeepkhatry <mandeepkhatry1997@gmail.com>
2018-07-21 14:35:59 +0545

    Add testmodels of useraccount

--- qrscanner/admin.py
+++ qrscanner/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Testuser
 # Register your models here.
+
+admin.site.register(Testuser)
--- qrscanner/models.py
+++ qrscanner/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Testuser(models.Model):
+    user_id = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
