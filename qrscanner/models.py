8ad30b153678044108d36b320b7baf6723cdd185
mandeepkhatry <mandeepkhatry1997@gmail.com>
2018-07-21 19:32:38 +0545

    Send request to server using Ajax

--- qrscanner/models.py
+++ qrscanner/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Testuser(models.Model):
-    user_id = models.CharField(max_length=200)
-    first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
--- qrscanner/views.py
+++ qrscanner/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def scan(request):
+    if request.method =="POST" :
+        print(request.POST.get('code'))
     return render(request,'qrscan.html');
