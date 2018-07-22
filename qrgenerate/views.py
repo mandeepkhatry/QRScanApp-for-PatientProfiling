1ef2ae4145eec2d970ddedaf2d6e53271ae9f977
mandeepkhatry <mandeepkhatry1997@gmail.com>
2018-07-22 13:24:15 +0545

    Test qr generate app 2

--- qrgenerate/views.py
+++ qrgenerate/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def generate(request):
+
+    #Test user id
+    #Use currently logged in user's userid
     user_id = "100nepal"
     return render(request,'qrgenerate.html',{'user_id':user_id})
